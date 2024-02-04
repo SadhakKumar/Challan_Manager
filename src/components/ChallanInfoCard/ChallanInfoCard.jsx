7042a4698cd3f9684cb9631726d89042215b5e33
sadhak <sadhak2003@gmail.com>
2024-02-04 17:40:14 +0530

    card scss done

--- src/components/ChallanInfoCard/ChallanInfoCard.jsx
+++ src/components/ChallanInfoCard/ChallanInfoCard.jsx
@@ -4,23 +4,39 @@ import "@fontsource/poppins";
 import Button from "../button/Button";
 
 const Challan = () => {
+  const handleChallengeClick = () => {
+    console.log("Challenge Clicked");
+  }
   return (
     <div className="challan">
-      <div className="challan__image"></div>
-      <img src="https://imgs.search.brave.com/WQmdk3AbP4loUv3GkEKUgqbFGUoRpX4b_m5tNx6WaWU/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudW5zcGxhc2gu/Y29tL3Bob3RvLTE1/NjUwNDM2NjY3NDct/NjlmNjY0NmRiOTQw/P3E9ODAmdz0xMDAw/JmF1dG89Zm9ybWF0/JmZpdD1jcm9wJml4/bGliPXJiLTQuMC4z/Jml4aWQ9TTN3eE1q/QTNmREI4TUh4elpX/RnlZMmg4TW54OGRt/Vm9hV05zWlhOOFpX/NThNSHg4TUh4OGZE/QT0.jpeg" alt="car" />
-      <div className="challan__info">
-        <span className="challan_id">Challan Number: BT530A1340</span>
-        <span className="amount">Amount: Rs. 2500</span>
-        <p>Reason: Red Light Skip</p>
-        <p>Location: Ulhasnagar, Thane. 421003</p>
-        
+      <div className="challanimg">
+        <img src="https://imgs.search.brave.com/WQmdk3AbP4loUv3GkEKUgqbFGUoRpX4b_m5tNx6WaWU/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudW5zcGxhc2gu/Y29tL3Bob3RvLTE1/NjUwNDM2NjY3NDct/NjlmNjY0NmRiOTQw/P3E9ODAmdz0xMDAw/JmF1dG89Zm9ybWF0/JmZpdD1jcm9wJml4/bGliPXJiLTQuMC4z/Jml4aWQ9TTN3eE1q/QTNmREI4TUh4elpX/RnlZMmg4TW54OGRt/Vm9hV05zWlhOOFpX/NThNSHg4TUh4OGZE/QT0.jpeg" alt="car" />
       </div>
-      <div className="RHS_container">
-        <div className="challan__buttons">
-            <Button color="#100775">Challenge</Button>
-            <Button color="#100775">Pay</Button>
+      <div className="rightside">
+        <div className="rightcontainer">
+          <div className="challan__info">
+            <span className="challan_id">Challan Number: BT530A1340</span>
+            <p>Reason: Red Light Skip</p>
+            <p>Location: Ulhasnagar, Thane. 421003</p>
+            
+          </div>
+          
+          <div className="amount">
+            <div className="">Amount: Rs. 2500</div>
+          </div>
+          
         </div>
-       </div>
+
+        <hr className="line"/>
+
+        <div className="buttons">
+          <Button children="Challenge" onClick= {handleChallengeClick} color ="#ffffff"/>
+          <Button children="Pay" onClick = {handleChallengeClick} color= "#100775"/>
+        </div>
+
+      </div>
+      
+
     </div>
   );
 };
--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -4,7 +4,7 @@ import './Button.scss'
 function Button({ children,onClick, color}) {
   const buttonStyle = {
     backgroundColor: color,
-    
+    color: color === "#ffffff" ? "black" : "white",
   };
     return (
         <button style={buttonStyle} className="button" onClick={onClick}>
