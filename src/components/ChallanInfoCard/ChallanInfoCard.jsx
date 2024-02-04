d1e6ebb0e49f37ee4537d8c8c5cb750dd662bea0
Learnedprawn <vineshparalkar@gmail.com>
2024-02-04 17:47:59 +0530

    changes

--- src/components/ChallanInfoCard/ChallanInfoCard.jsx
+++ src/components/ChallanInfoCard/ChallanInfoCard.jsx
@@ -6,21 +6,22 @@ import Button from "../button/Button";
 const Challan = () => {
   return (
     <div className="challan">
-      <div className="challan__image"></div>
-      <img src="https://imgs.search.brave.com/WQmdk3AbP4loUv3GkEKUgqbFGUoRpX4b_m5tNx6WaWU/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudW5zcGxhc2gu/Y29tL3Bob3RvLTE1/NjUwNDM2NjY3NDct/NjlmNjY0NmRiOTQw/P3E9ODAmdz0xMDAw/JmF1dG89Zm9ybWF0/JmZpdD1jcm9wJml4/bGliPXJiLTQuMC4z/Jml4aWQ9TTN3eE1q/QTNmREI4TUh4elpX/RnlZMmg4TW54OGRt/Vm9hV05zWlhOOFpX/NThNSHg4TUh4OGZE/QT0.jpeg" alt="car" />
-      <div className="challan__info">
-        <span className="challan_id">Challan Number: BT530A1340</span>
-        <span className="amount">Amount: Rs. 2500</span>
-        <p>Reason: Red Light Skip</p>
-        <p>Location: Ulhasnagar, Thane. 421003</p>
-        
+      <div className="info_image">
+        <div className="challan__image">
+          <img src="https://imgs.search.brave.com/WQmdk3AbP4loUv3GkEKUgqbFGUoRpX4b_m5tNx6WaWU/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudW5zcGxhc2gu/Y29tL3Bob3RvLTE1/NjUwNDM2NjY3NDct/NjlmNjY0NmRiOTQw/P3E9ODAmdz0xMDAw/JmF1dG89Zm9ybWF0/JmZpdD1jcm9wJml4/bGliPXJiLTQuMC4z/Jml4aWQ9TTN3eE1q/QTNmREI4TUh4elpX/RnlZMmg4TW54OGRt/Vm9hV05zWlhOOFpX/NThNSHg4TUh4OGZE/QT0.jpeg" alt="car" />
+
+        </div>
+        <div className="challan__info">
+          <span className="challan_id">Challan Number: BT530A1340</span>
+          <span className="amount">Amount: Rs. 2500</span>
+          <p>Reason: Red Light Skip</p>
+          <p>Location: Ulhasnagar, Thane. 421003</p>
+        </div>
       </div>
-      <div className="RHS_container">
         <div className="challan__buttons">
             <Button color="#100775">Challenge</Button>
             <Button color="#100775">Pay</Button>
         </div>
-       </div>
     </div>
   );
 };
