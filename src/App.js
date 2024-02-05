b4da5b5d92e18fa7f08921e54d153376773cdd9c
sadhak <sadhak2003@gmail.com>
2024-02-05 20:28:40 +0530

    working on upload proof card

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Routes,Route } from 'react-router-dom';
 import SearchLayout from './components/SearchLayout/SearchLayout';
 import ChallanInfoCard from "./components/ChallanInfoCard/ChallanInfoCard"
+import UploadProofCard from './components/UploadProofCard/UploadProofCard';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path = '/' element = {<SearchLayout/>}/>
         <Route path = '/challan' element = {<ChallanInfoCard/>}/>
+        <Route path = '/upload' element = {<UploadProofCard/>}/>
       </Routes>
     
   );
