2a1b5ba7ef1717b8b0970c74903a8642138bb46a
Ravi Valecha <ravivalecha2510@gmail.com>
2024-02-08 21:43:44 +0530

    add: added search input and button to add challenge

--- src/components/Police/PoliceDashboard/PoliceDashboard.jsx
+++ src/components/Police/PoliceDashboard/PoliceDashboard.jsx
@@ -4,17 +4,34 @@ import Challan from '../ChallanInfoCard/ChallanInfoCard'
 import Navbar from '../../Navbar/Navbar'
 import jsonData from '../../../utils/challan.json'
 import "./PoliceDashboard.scss";
+import { Link } from 'react-router-dom'
+import Button from '../../button/Button'
 
 const PoliceDashboard = () => {
   return (
     <>
         <Navbar />
-        <div className='grid'>
-          {jsonData.map((data, index) => (
-            <div className='item'>
-              <Challan key={index} data={data} />
-            </div>)
-          )}
+        <div className="police_container">
+          <div className="vehicle-number-input">
+              <input
+                type="text"
+                id="searchByVehicleNumber"
+                name="searchByVehicleNumber"
+                placeholder='Search by Vehicle Number'
+              />
+          </div>
+          <div className='grid'>
+            {jsonData.map((data, index) => (
+              <div className='item'>
+                <Challan key={index} data={data} />
+              </div>)
+            )}
+          </div>
+            <div className="add-challenge-button">
+              <Link to="/">
+                <Button children="Add Challenge" onClick={()=>console.log("Challenge added!")} color ="#100775"/>
+              </Link>
+            </div>
         </div>
         <Footer />
     </>
