c21204159eee3210dc92f2f61f59387303877608
Ravi Valecha <ravivalecha2510@gmail.com>
2024-02-08 20:09:26 +0530

    create: created dashboard page with challan cards

--- src/components/Police/PoliceDashboard/PoliceDashboard.jsx
+++ src/components/Police/PoliceDashboard/PoliceDashboard.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Footer from '../../Footer/Footer'
+import Challan from '../ChallanInfoCard/ChallanInfoCard'
+import Navbar from '../../Navbar/Navbar'
+import jsonData from '../../../utils/challan.json'
+import "./PoliceDashboard.scss";
 
 const PoliceDashboard = () => {
   return (
-    <div>PoliceDashboard</div>
+    <>
+        <Navbar />
+        <div className='grid'>
+          {jsonData.map((data, index) => (
+            <div className='item'>
+              <Challan key={index} data={data} />
+            </div>)
+          )}
+        </div>
+        <Footer />
+    </>
+
   )
 }
 
