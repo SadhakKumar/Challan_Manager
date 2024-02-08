821ee13bcf378f17da00a1d50a5303364884ef99
Learnedprawn <vineshparalkar@gmail.com>
2024-02-09 00:00:21 +0530

    Police login, Admin Dashboard, Admin Login

--- src/components/Admin/AdminDashboard/AdminDashboard.jsx
+++ src/components/Admin/AdminDashboard/AdminDashboard.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import Footer from '../../Footer/Footer'
+import Challan from '../../Police/ChallanInfoCard/ChallanInfoCard'
+import Navbar from '../../Navbar/Navbar'
+import jsonData from '../../../utils/challan.json'
+import "./AdminDashboard.scss";
+import { Link } from 'react-router-dom'
+import Button from '../../button/Button'
 
 const AdminDashboard = () => {
   return (
-    <div>AdminDashboard</div>
+    <>
+        <Navbar />
+        <div className="police_container">
+          {/* <div className="vehicle-number-input">
+              <input
+                type="text"
+                id="searchByVehicleNumber"
+                name="searchByVehicleNumber"
+                placeholder='Search by Vehicle Number'
+              />
+          </div> */}
+          <div className='grid'>
+            {jsonData.map((data, index) => (
+              <div className='item'>
+                <Challan key={index} data={data} />
+              </div>)
+            )}
+          </div>
+            <div className="add-challenge-button">
+              <Link to="/">
+                <Button children="Add Challenge" onClick={()=>console.log("Challenge added!")} color ="#100775"/>
+              </Link>
+            </div>
+        </div>
+        <Footer />
+    </>
+
   )
 }
 
