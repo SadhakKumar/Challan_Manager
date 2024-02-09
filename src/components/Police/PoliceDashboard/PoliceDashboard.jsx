7b41e4910df9d7f53d83888be466b7bfa1949eb8
Ravi <ravivalecha2510@gmail.com>
2024-02-09 20:18:36 +0530

    add: display challan cards as per searched vehicle number

--- src/components/Police/PoliceDashboard/PoliceDashboard.jsx
+++ src/components/Police/PoliceDashboard/PoliceDashboard.jsx
@@ -1,27 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Footer from '../../Footer/Footer'
 import Challan from '../ChallanInfoCard/ChallanInfoCard'
 import Navbar from '../../Navbar/Navbar'
-import jsonData from '../../../utils/challan.json'
 import "./PoliceDashboard.scss";
 import { Link } from 'react-router-dom'
 import Button from '../../button/Button'
+import axios from '../../../api/axios';
 
 const PoliceDashboard = () => {
+
+  const[challans, setChallans] = useState([]);
+
+  const handleSubmit = async (evt) => {
+    evt.preventDefault();
+    const vehicleNo = evt.target[0].value;
+    const responseChallans = await axios.get(`/challans?vehicle_no=${vehicleNo}`);
+    setChallans(responseChallans.data[0].challans);
+  }
+  
   return (
     <>
         <Navbar />
         <div className="police_container">
-          <div className="vehicle-number-input">
+          <form className="vehicle-number-input" onSubmit={evt=>handleSubmit(evt)}>
               <input
                 type="text"
                 id="searchByVehicleNumber"
                 name="searchByVehicleNumber"
                 placeholder='Search by Vehicle Number'
               />
-          </div>
+          </form>
           <div className='grid'>
-            {jsonData.map((data, index) => (
+            {challans.map((data, index) => (
               <div className='item'>
                 <Challan key={index} data={data} />
               </div>)
@@ -29,7 +39,7 @@ const PoliceDashboard = () => {
           </div>
             <div className="add-challenge-button">
               <Link to="/">
-                <Button children="Add Challenge" onClick={()=>console.log("Challenge added!")} color ="#100775"/>
+                <Button children="Add Challan" onClick={()=>console.log("Challenge added!")} color ="#100775"/>
               </Link>
             </div>
         </div>
