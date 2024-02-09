f979cf71c3f464ccc7d92a8171aff02b4505b02e
Ravi <ravivalecha2510@gmail.com>
2024-02-09 19:36:33 +0530

    refactor: refactored ViewChallanLayout code

--- src/components/ViewChallanLayout/ViewChallanLayout.jsx
+++ src/components/ViewChallanLayout/ViewChallanLayout.jsx
@@ -2,34 +2,24 @@ import React,{useEffect, useState} from 'react'
 import Footer from '../Footer/Footer'
 import Challan from '../ChallanInfoCard/ChallanInfoCard'
 import Navbar from '../Navbar/Navbar'
-import jsonData from '../../utils/challan.json'
-import { useSelector,useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
 import {getVehicleNo, getChassisNo} from '../../features/Search/SearchSlice'
 import axios from '../../api/axios'
 
 const ViewChallanLayout = () => {
 
-  const dispatch = useDispatch();
-  const [vehicleNumber, setVehicleNumber] = useState('');
-  const [chassisNumber, setChassisNumber] = useState('');
   const [challans, setChallans] = useState([]);
 
   const vehicleNo = useSelector(getVehicleNo);
   const chassisNo = useSelector(getChassisNo);
 
-
-  const getChallans = async() =>{
-    const response = await axios.get('/challans?vehicle_no='+vehicleNo+'&chassis_no='+chassisNo);
-    setChallans(response.data);
-  }
-
   useEffect(() => {
-    setVehicleNumber(vehicleNo);
-    setChassisNumber(chassisNo);
-   
+    const getChallans = async() =>{
+      const response = await axios.get('/challans?vehicle_no='+vehicleNo+'&chassis_no='+chassisNo);
+      setChallans(response.data);
+    }
     getChallans();
-    
-  },[dispatch,vehicleNo,chassisNo])
+  },[chassisNo, vehicleNo]);
 
   console.log(challans);
 
