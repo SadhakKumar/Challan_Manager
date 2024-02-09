5735d4da4f1405c9804553f81ff7b68036af068b
Ravi <ravivalecha2510@gmail.com>
2024-02-09 20:58:37 +0530

    add: added car card as per searched vehicle number

--- src/components/Police/PoliceDashboard/PoliceDashboard.jsx
+++ src/components/Police/PoliceDashboard/PoliceDashboard.jsx
@@ -6,16 +6,20 @@ import "./PoliceDashboard.scss";
 import { Link } from 'react-router-dom'
 import Button from '../../button/Button'
 import axios from '../../../api/axios';
+import CarCard from '../CarCard/CarCard';
 
 const PoliceDashboard = () => {
 
-  const[challans, setChallans] = useState([]);
+  const[challans, setChallans] = useState(null);
+  const[car, setCar] = useState(null);
 
   const handleSubmit = async (evt) => {
     evt.preventDefault();
     const vehicleNo = evt.target[0].value;
     const responseChallans = await axios.get(`/challans?vehicle_no=${vehicleNo}`);
-    setChallans(responseChallans.data[0].challans);
+    setChallans(responseChallans?.data[0]?.challans??null);
+    const responseCar = await axios.get(`/cars?vehicle=${vehicleNo}`);
+    setCar(responseCar?.data[0]??null);
   }
   
   return (
@@ -30,8 +34,12 @@ const PoliceDashboard = () => {
                 placeholder='Search by Vehicle Number'
               />
           </form>
+          <div>
+            {car ? <CarCard data={car} /> : <h1>No Vehicle Found</h1> }
+          </div>
+          { car && (challans ? <h1>Challans Found: {challans.length}</h1> : <h1>No Challans Found</h1>) }
           <div className='grid'>
-            {challans.map((data, index) => (
+            {challans && challans.map((data, index) => (
               <div className='item'>
                 <Challan key={index} data={data} />
               </div>)
