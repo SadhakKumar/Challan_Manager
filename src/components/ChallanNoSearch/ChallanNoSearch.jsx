59be866e7b78c9dd7037c1eade040bba345f9251
ErMaanasR <maanasruchandani77@gmail.com>
2024-03-08 19:48:47 +0530

    changed ui

--- src/components/ChallanNoSearch/ChallanNoSearch.jsx
+++ src/components/ChallanNoSearch/ChallanNoSearch.jsx
@@ -1,6 +1,6 @@
-import React,{useState} from 'react'
-import ReCAPTCHA from 'react-google-recaptcha'
-import './ChallanNoSearch.scss'
+import React, { useState } from 'react';
+import ReCAPTCHA from 'react-google-recaptcha';
+import './ChallanNoSearch.scss';
 import Button from '../button/Button';
 import RadioButtonSearch from '../RadioButtonSearch.jsx/RadioButtonSearch';
 import { useNavigate } from 'react-router';
@@ -12,34 +12,37 @@ function ChallanNoSearch() {
     const handleSubmit = () => {
         console.log("pressed");
         navigate('/view');
-    }
+    };
+
     return (
         <div className="container">
-          <form onSubmit={handleSubmit}>
-            <RadioButtonSearch/>
-            <div className="input-group">
-              <input
-                type="text"
-                id="vehicleNumber"
-                name="vehicleNumber"
-                placeholder='Enter Challan Number'
-                value={challanNumber}
-                onChange={(e) => setChallanNumber(e.target.value)}
-                required
-              />
-            </div>
-  
-            <div className='recaptcha'>
-              <ReCAPTCHA
-                  sitekey="6LeIxAcTAAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe"
-                />
-            </div>
-            
-            <Button children ='Submit' onClick = {handleSubmit} color = '#1C3AA9'/>
-          </form>
+            <form onSubmit={handleSubmit}>
+                <RadioButtonSearch />
+                <div className="input-group">
+                    <label htmlFor="vehicleNumber">Challan Number:</label>
+                    <input
+                        type="text"
+                        id="vehicleNumber"
+                        name="vehicleNumber"
+                        placeholder='Enter Challan Number'
+                        value={challanNumber}
+                        onChange={(e) => setChallanNumber(e.target.value)}
+                        required
+                    />
+                </div>
+
+                <div className='recaptcha'>
+                    <ReCAPTCHA
+                        sitekey="6LeIxAcTAAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe"
+                    />
+                </div>
+
+                <div className='button-group'>
+                    <Button children='Submit' onClick={handleSubmit} />
+                </div>
+            </form>
         </div>
-        
-      );
+    );
 }
 
-export default ChallanNoSearch
+export default ChallanNoSearch;
