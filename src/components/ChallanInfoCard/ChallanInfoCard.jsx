ea7f6ecd7c7e2cbd49e6f1747c778957840b6b21
SadhakKumar <sadhak2003@gmail.com>
2024-04-13 00:26:20 +0530

    added razorpay

--- src/components/ChallanInfoCard/ChallanInfoCard.jsx
+++ src/components/ChallanInfoCard/ChallanInfoCard.jsx
@@ -4,17 +4,50 @@ import "@fontsource/poppins";
 import Button from "../button/Button";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
+import useRazorpay from "react-razorpay";
+import axios from "axios";
+
 
 const Challan = ({data, isPending=true}) => {
+  const [Razorpay, isLoaded] = useRazorpay();
+
   console.log(data)
   const navigate = useNavigate();
   const handleChallengeClick = () => {
     console.log("Challenge Clicked");
     // navigate("/challenge");
   }
-  const handlePayClick = () => {
-    console.log("Pay Clicked");
+  const handlePayClick = async(e) => {
+    const API_URL = 'http://localhost:3000/';
+    e.preventDefault();
+    const orderUrl = `${API_URL}order`;
+    const response = await axios.get(orderUrl);
+    const { data } = response;
+    console.log(data);
+    const options = {
+      key: "rzp_test_euZ7U1egQIuWP9",
+      name: "sadhak",
+      description: "Payment",
+      order_id: data.id,
+      handler: async (response) => {
+        try {
+        const paymentId = response.razorpay_payment_id;
+        const url = `${API_URL}capture/${paymentId}`;
+        const captureResponse = await axios.post(url, {})
+        console.log(captureResponse.data);
+        } catch (err) {
+          console.log(err);
+        }
+      },
+      theme: {
+        color: "#686CFD",
+      },
+    };
+    const rzp1 = new window.Razorpay(options);
+    rzp1.open();
+
   }
+
   return (
     <div className="challan">
       <div className="">
